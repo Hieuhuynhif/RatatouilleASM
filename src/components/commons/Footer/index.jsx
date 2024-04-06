6292b3190dacfe8f56bf77fb94eabc51b7a9b500
hieuhuynhif <hieuhuynhif@gmail.com>
2024-04-06 13:15:36 +0700

    layout

--- src/components/commons/Footer/index.jsx
+++ src/components/commons/Footer/index.jsx
@@ -1,16 +1,7 @@
-import React from 'react';
-import {  Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 
-Footer.propTypes = {
-    
-};
-
-function Footer(props) {
-    return (
-        <Stack height={"10vh"}>
-            
-        </Stack>
-    );
+function Footer() {
+  return <Stack height={"10vh"}></Stack>;
 }
 
 export default Footer;
