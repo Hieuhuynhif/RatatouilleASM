54b6fd64fa77eb8a772a9ff47ae57176b79e33ec
bao2128 <quocbao2128@gmail.com>
2024-04-06 15:38:11 +0700

    ingredient list

---
+++ src/components/commons/List ingredient/index.jsx
@@ -0,0 +1,90 @@
+import { MoreVert } from "@mui/icons-material";
+import { CardActionArea, CardHeader, IconButton } from "@mui/material";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import PropTypes from "prop-types";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+
+ActionAreaCard.propTypes = {
+  ingredients: PropTypes.arrayOf(Object),
+};
+
+export default function ActionAreaCard(props) {
+  const { ingredients } = props;
+
+  const itemsPerPage = 5;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
+
+  const getPageData = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return ingredients.slice(startIndex, endIndex);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {getPageData().map((item) => (
+          <Grid item xs={2} sm={4} md={4} key={item.name}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardHeader
+                action={
+                  <IconButton aria-label="settings">
+                    <MoreVert />
+                  </IconButton>
+                }
+                title={item.name.toUpperCase()}
+              />
+
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image="src\assets\p1.jpg"
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography variant="body2" color="text.secondary">
+                    Description:
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Expire date: {item.exp_date}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Quantity: {item.quantity} kg
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      <Stack spacing={2} alignItems={"end"} mt={2}>
+        <Pagination
+          color="primary"
+          count={Math.ceil(ingredients.length / itemsPerPage)} // Total number of pages
+          page={currentPage}
+          onChange={handlePageChange}
+          defaultPage={1}
+          boundaryCount={2}
+          showFirstButton
+          showLastButton
+        />
+      </Stack>
+    </Box>
+  );
+}
--- src/pages/Ingredients/index.jsx
+++ src/pages/Ingredients/index.jsx
@@ -1,11 +1,96 @@
-import React from 'react'
+import React from "react";
+import ActionAreaCard from "../../components/commons/List Ingredients";
 
 function Ingredients(props) {
+  const list = [
+    {
+      name: "Cá ngừ",
+      pic: "/sdfhbs/sdfdshjb",
+      quantity: "12",
+      exp_date: "01/04/24",
+      imp_date: "01/04/24",
+      price: "100000",
+    },
+    {
+      name: "Cá hồi",
+      pic: "fvjnxf/fgbjkfnbd",
+      quantity: "25",
+      exp_date: "20/03/24",
+      imp_date: "01/04/24",
+      price: "100000",
+    },
+    {
+      name: "Cá saba",
+      pic: "",
+      quantity: "8",
+      exp_date: "02/04/24",
+      imp_date: "01/04/24",
+      price: "100000",
+    },
+    {
+      name: "Cá lóc",
+      pic: "",
+      quantity: "5",
+      exp_date: "10/02/24",
+      imp_date: "01/04/24",
+      price: "100000",
+    },
+    {
+      name: "Cá Bống",
+      pic: "",
+      quantity: "35",
+      exp_date: "07/02/24",
+      imp_date: "01/04/24",
+      price: "100000",
+    },
+    {
+      name: "Thịt heo",
+      pic: "",
+      quantity: "65",
+      exp_date: "10/02/24",
+      imp_date: "01/04/24",
+      price: "100000",
+    },
+    {
+      name: "Thịt bò",
+      pic: "",
+      quantity: "65",
+      exp_date: "10/02/24",
+      imp_date: "01/04/24",
+      price: "100000",
+    },
+    {
+      name: "Thịt gà",
+      pic: "",
+      quantity: "9",
+      exp_date: "09/03/24",
+      imp_date: "01/04/24",
+      price: "100000",
+    },
+    {
+      name: "Thịt cừu",
+      pic: "",
+      quantity: "6",
+      exp_date: "09/02/24",
+      imp_date: "01/04/24",
+      price: "100000",
+    },
+    {
+      name: "Cua hoàng đế",
+      pic: "",
+      quantity: "19",
+      exp_date: "20/03/24",
+      imp_date: "01/04/24",
+      price: "100000",
+    },
+  ];
   return (
-    <div>ingredients</div>
-  )
+    <>
+      <ActionAreaCard ingredients={list} />
+    </>
+  );
 }
 
-Ingredients.propTypes = {}
+Ingredients.propTypes = {};
 
-export default Ingredients
+export default Ingredients;
